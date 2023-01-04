4b36b4ee325f3f733aabbd1a22821bbe36c0742d
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-01-04 12:32:11 -0300

    Organizando tabela simulador

--- simulador_de_campeonato.cpp
+++ simulador_de_campeonato.cpp
@@ -3,7 +3,7 @@
 #include<locale.h>
 #include<time.h>
 #include<string.h>
-int pts[20], golsTime[2], jogos[20], vit[20], emp[20], gp[20], gc[20];
+int pts[20], golsTime[2], posicao[20], jogos[20], vit[20], emp[20], gp[20], gc[20], aux;
 void naoEmpate(int ganhador){
 	vit[ganhador]++;
 	pts[ganhador] += 3;
@@ -11,9 +11,10 @@ void naoEmpate(int ganhador){
 int main(void){
 	setlocale(LC_ALL, "portuguese");
 	srand(time(NULL));
-	char timesParticipantes[20][20] = {"America Mineiro", "Athletico", "Atletico Mineiro", "Bahia", "Botafogo", "Red Bull Bragantino", "Corinthians", "Coritiba", "Cruzeiro", "Cuiaba", "Flamengo", "Fluminense", "Fortaleza", "Goiás", "Gremio", "Internacional", "Palmeiras", "Santos", "São Paulo", "Vasco"};
-	float aproveitamento[20];
+	char timesParticipantes[20][20] = {"AME", "CAP", "CAM", "BAH", "BOT", "RBB", "COR", "CTB", "CRU", "CUI", "FLA", "FLU", "FOR", "GOI", "GRE", "INT", "PAL", "SAN", "SPO", "VAS"};
+	double aproveitamento[20];
 	for(int i = 0;i < 20;i++){
+		posicao[i] = i;
 		for(int j = i + 1;j < 20;j++){
 			for(int k = 0;k < 2;k++){
 				golsTime[0] = rand() %3;
@@ -35,11 +36,21 @@ int main(void){
 				}
 			}
 		}
-		printf("aaaaa");
 	}
+	for(int i = 0;i < 20;i++){ // calcula o aproveitamento e ordena os times
+		aproveitamento[i] = 100.0 * pts[i] / (3 * jogos[i]);
+		for(int j = 0;j < 20;j++){
+			if(pts[posicao[i]] > pts[posicao[j]] || pts[posicao[i]] == pts[posicao[j]] && (vit[posicao[i]] > vit[posicao[j]] || vit[posicao[i]] == vit[posicao[j]] && (gp[posicao[i]] - gc[posicao[i]] > gp[posicao[j]] - gc[posicao[j]] || gp[posicao[i]] - gc[posicao[i]] == gp[posicao[j]] - gc[posicao[j]] && (gp[posicao[i]] > gc[posicao[j]])))){
+				aux = posicao[i];
+				posicao[i] = posicao[j];
+				posicao[j] = aux;
+			}
+			
+		}
+	}
+	printf("Pos\tTime\tPts\tJogos\tVit\tEmp\tDer\tGP\tGC\tSG\tAproveitamento");
 	for(int i = 0;i < 20;i++){
-		aproveitamento[i] = pts[i] / 3 / jogos[i];
-		printf("\n%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f", /*timesParticipantes[i],*/ pts[i], jogos[i], vit[i], emp[i], jogos[i] - vit[i] - emp[i], gp[i], gc[i], gp[i] - gc[i], aproveitamento[i]);
+		printf("\n%d\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%.2f", i + 1, timesParticipantes[posicao[i]], pts[posicao[i]], jogos[posicao[i]], vit[posicao[i]], emp[posicao[i]], jogos[posicao[i]] - vit[posicao[i]] - emp[posicao[i]], gp[posicao[i]], gc[posicao[i]], gp[posicao[i]] - gc[posicao[i]], aproveitamento[posicao[i]]);
 	}
 	return 0;
 }
